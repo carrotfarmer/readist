a4ea01db5c453704ef08279eb6bca6aa5397cf73
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-04-23 21:03:20 +0530

    feat: page layout

--- src/pages/readinglist/[rlId].tsx
+++ src/pages/readinglist/[rlId].tsx
@@ -1,16 +1,38 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { Navbar } from "~/components/nav/Navbar";
 
 const ReadingListPage: NextPage = () => {
   const router = useRouter();
   const { rlId } = router.query;
 
+  const { data: session } = useSession();
+
   return (
     <Box>
-      hello {rlId}
+      <Head>
+        <title>my reading list</title>
+        <meta name="description" content="my reading list" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Navbar />
+
+        {session ? (
+          <div>hello</div>
+        ) : (
+          <Center>
+            <Box pt="5">
+              <Text>not authenticated. please sign in to continue.</Text>
+            </Box>
+          </Center>
+        )}
+      </main>
     </Box>
-  )
-}
+  );
+};
 
 export default ReadingListPage;
