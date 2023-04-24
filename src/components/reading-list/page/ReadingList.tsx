a2cda025c53cfa5698dae59d1b9ff785e5e27c71
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-04-24 13:52:57 +0530

    feat: rl actn btns

--- src/components/reading-list/page/ReadingList.tsx
+++ src/components/reading-list/page/ReadingList.tsx
@@ -1,6 +1,9 @@
-import { Box, Center, Heading } from '@chakra-ui/react';
-import type React from 'react'
-import { IReadingList } from '~/types';
+import { Box, Center, Heading, HStack, Button } from "@chakra-ui/react";
+import type React from "react";
+import { IReadingList } from "~/types";
+
+import { BsTrash as DeleteIcon } from "react-icons/bs";
+import { BiEdit as EditIcon } from "react-icons/bi";
 
 interface ReadingListProps {
   readingList: IReadingList;
@@ -10,10 +13,25 @@ export const ReadingList: React.FC<ReadingListProps> = ({ readingList }) => {
   return (
     <Box pt="10">
       <Center>
-        <Heading>
-          {readingList.name}
-        </Heading>
+        <Heading>{readingList.name}</Heading>
       </Center>
+      <Center pt="1%">
+        <HStack gap="1%">
+          <Button colorScheme="orange" size="xs">
+            <Box pr="1">
+              <EditIcon />
+            </Box>
+            Edit
+          </Button>
+          <Button colorScheme="red" size="xs">
+            <Box pr="1">
+              <DeleteIcon />
+            </Box>
+            Delete
+          </Button>
+        </HStack>
+      </Center>
+      <Center></Center>
     </Box>
-  )
-}
+  );
+};
