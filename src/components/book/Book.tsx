d7ec8578c66093d4678764465fe0c8b879423bde
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-04-25 15:55:53 +0530

    feat: edit book

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -4,11 +4,21 @@ import {
   HStack,
   Checkbox,
   useColorModeValue,
+  useDisclosure,
   Spacer,
   Button,
   ButtonGroup,
+  PopoverTrigger,
+  Popover,
+  Portal,
+  PopoverContent,
+  PopoverArrow,
+  PopoverHeader,
+  PopoverCloseButton,
+  PopoverBody,
 } from "@chakra-ui/react";
 import type React from "react";
+import { useRef } from "react";
 
 import { BsTrash as DeleteIcon } from "react-icons/bs";
 import { BiEdit as EditIcon } from "react-icons/bi";
@@ -16,6 +26,7 @@ import { BiEdit as EditIcon } from "react-icons/bi";
 import type { Book as IBook } from "@prisma/client";
 import { useBookStore } from "~/store/BookStore";
 import { api } from "~/utils/api";
+import { EditBookForm } from "./EditBookForm";
 
 interface BookProps {
   book: IBook;
@@ -29,6 +40,10 @@ export const Book: React.FC<BookProps> = ({ book }) => {
     },
   });
 
+  // edit popover
+  const { isOpen: isPopoverOpen, onOpen: onPopoverOpen, onClose: onPopoverClose } = useDisclosure();
+  const editRef = useRef(null);
+
   return (
     <HStack pb="5">
       <Checkbox mr="10" borderColor={useColorModeValue("gray.400", "gray.500")} />
@@ -36,11 +51,33 @@ export const Book: React.FC<BookProps> = ({ book }) => {
       <Text fontWeight="light">{book.author}</Text>
       <Spacer />
       <ButtonGroup>
-        <Button colorScheme="orange" size="xs">
-          <Box>
-            <EditIcon />
-          </Box>
-        </Button>
+        <Popover
+          isOpen={isPopoverOpen}
+          onOpen={onPopoverOpen}
+          onClose={onPopoverClose}
+          closeOnBlur={false}
+          initialFocusRef={editRef}
+        >
+          <PopoverTrigger>
+            <Button colorScheme="orange" size="xs">
+              <Box>
+                <EditIcon />
+              </Box>
+            </Button>
+          </PopoverTrigger>
+
+          <Portal>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverHeader>Edit Reading List Name</PopoverHeader>
+              <PopoverCloseButton tabIndex={2} />
+              <PopoverBody>
+                <EditBookForm editRef={editRef} onCancel={onPopoverClose} book={book} />
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </Popover>
+
         <Button colorScheme="red" size="xs" onClick={() => removeBook({ bookId: book.id })}>
           <Box>
             <DeleteIcon />
---
+++ src/components/book/EditBookForm.tsx
@@ -0,0 +1,113 @@
+import type React from "react";
+import { type MutableRefObject } from "react";
+
+import {
+  Button,
+  ButtonGroup,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Stack,
+  useMergeRefs,
+} from "@chakra-ui/react";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import type { Book } from "@prisma/client";
+import { api } from "~/utils/api";
+
+interface EditBookFormProps {
+  editRef: MutableRefObject<null>;
+  onCancel: () => void;
+  book: Book;
+}
+
+const formSchema = z.object({
+  newTitle: z
+    .string()
+    .min(5, { message: "min length for the book title is 5 chars" })
+    .max(200, { message: "max length for book title is 200 chars" }),
+
+  newAuthor: z
+    .string()
+    .min(2, { message: "min length for the author is 2 chars" })
+    .max(200, { message: "max length for book title is 200 chars" }),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
+export const EditBookForm: React.FC<EditBookFormProps> = ({ editRef, onCancel, book }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting, errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    mode: "onTouched",
+  });
+
+  const { mutate: editBook } = api.book.editBook.useMutation({
+    onSuccess: () => {
+      window.location.reload();
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    editBook({ bookId: book.id, newTitle: data.newTitle, newAuthor: data.newAuthor });
+  };
+
+  return (
+    <Stack spacing={4}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={Boolean(errors.newTitle)}>
+          <FormLabel>Title</FormLabel>
+          <Input
+            type="text"
+            variant="filled"
+            focusBorderColor="teal.300"
+            tabIndex={1}
+            placeholder="Thinking, Fast and Slow"
+            defaultValue={book.name}
+            {...register("newTitle")}
+            ref={useMergeRefs(editRef, register("newTitle").ref)}
+          />
+          <FormErrorMessage>{errors.newTitle && errors.newTitle?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={Boolean(errors.newAuthor)} pt="4%">
+          <FormLabel>Author</FormLabel>
+          <Input
+            type="text"
+            placeholder="eg: Daniel Kahneman"
+            variant="filled"
+            focusBorderColor="teal.300"
+            tabIndex={1}
+            defaultValue={book.author}
+            {...register("newAuthor")}
+          />
+          <FormErrorMessage>{errors.newAuthor && errors.newAuthor?.message}</FormErrorMessage>
+        </FormControl>
+
+        <ButtonGroup display="flex" justifyContent="flex-end" pt="4%">
+          <Button
+            variant="outline"
+            onClick={() => {
+              reset();
+              onCancel();
+            }}
+            tabIndex={3}
+          >
+            Cancel
+          </Button>
+          <Button variant="outline" isLoading={isSubmitting} type="submit">
+            Edit
+          </Button>
+        </ButtonGroup>
+      </form>
+    </Stack>
+  );
+};
