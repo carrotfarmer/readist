233d3f7bd728b9078eeeff8b0ae16d3da386f5e3
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-04-25 15:14:40 +0530

    feat: delete book state

--- src/store/BookStore.ts
+++ src/store/BookStore.ts
@@ -1,27 +1,20 @@
 import type { Book } from "@prisma/client";
-import type { IReadingList } from "../types";
 import { create } from "zustand";
 
 interface State {
   books: Book[];
 }
 
-// interface Action {
-//   setReadingLists: (readingLists: IReadingList[]) => void;
-//   addReadingList: (readingList: IReadingList) => void;
-//   deleteReadingList: (readingListId: string) => void;
-// }
-//
 interface Action {
   setBooks: (books: Book[]) => void;
   addBook: (book: Book) => void;
+  deleteBook: (bookId: string) => void;
 }
 
 export const useBookStore = create<State & Action>((set) => ({
   books: [],
   setBooks: (books: Book[]) => set(() => ({ books })),
   addBook: (book: Book) => set((state) => ({ books: [...state.books, book] })),
+  deleteBook: (bookId: string) =>
+    set((state) => ({ books: state.books.filter((book) => book.id !== bookId) })),
 }));
-
-const unsub1 = useBookStore.subscribe(console.log)
-
