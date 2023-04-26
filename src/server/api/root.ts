d54988604537978445e9815611787fb87534cc45
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-04-26 22:01:39 +0530

    fix: user rl check procedure

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
 
 import { readingListRouter } from "~/server/api/routers/readingList";
 import { bookRouter } from "./routers/book";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -9,6 +10,7 @@ import { bookRouter } from "./routers/book";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  user: userRouter,
   readingList: readingListRouter,
   book: bookRouter,
 });
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  isRlPartOfUser: protectedProcedure
+    .input(z.object({ rlId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+        include: {
+          readingLists: true,
+        },
+      });
+
+      const userRl = user?.readingLists.filter((rl) => rl.id === input.rlId);
+
+      if (userRl) {
+        return true;
+      } else {
+        return false;
+      }
+    }),
+});
